2be9724fece5312895c7e6bec0ea075ea674a536
seniorjafar <147535212+seniorjafar@users.noreply.github.com>
2024-05-11 09:50:54 +0500

    Commit xabarini yozing

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./Users.css";
 import male from "../../assets/male-avatar-boy-face-man-user-9.svg";
 import female from "../../assets/female-avatar-girl-face-woman-user-2.svg";
 import { useDispatch } from 'react-redux';
 import { removeFromUsers } from "../../context/usersSlice";
+import Modal from 'react-modal';
 
 function Users({ data }) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
   const dispatch = useDispatch();
 
   const handleRemoveUser = (user) => {
@@ -14,6 +16,18 @@ function Users({ data }) {
 
   return (
     <div className='users__wrapper'>
+      <div>
+      
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={() => setModalIsOpen(false)}
+        
+      >
+        <h2>Personal</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam provident modi sequi consequatur quia porro praesentium quasi! Aliquid deleniti deserunt voluptatibus numquam repudiandae cupiditate culpa. Tenetur blanditiis excepturi reprehenderit in! Molestias rerum nam voluptates ea animi. Beatae, rem sunt ullam sit velit excepturi dolores, commodi nihil corrupti voluptates, numquam itaque!</p>
+        <button className='modal__btn' onClick={() => setModalIsOpen(false)}>Close</button>
+      </Modal>
+    </div>
       {data?.map((user) => (
         <div key={user.id} className="users__card">
           <img src={user.gender === "male" ? male : female} alt="" />
@@ -22,7 +36,10 @@ function Users({ data }) {
           <p>{user.age} years old</p>
           <p>+998 {user.number}</p>
           <p>{user.address}</p>
-          <button onClick={() => handleRemoveUser(user)}>Remove</button>
+          <div className='btn_code'>
+            <button onClick={() => handleRemoveUser(user)}>Remove</button>
+          <button onClick={() => setModalIsOpen(true)}>More</button>
+          </div>
         </div>
       ))}
     </div>
--- src/router/all-users/AllUsers.jsx
+++ src/router/all-users/AllUsers.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./AllUsers.css"
 import Users from '../../components/users/Users'
 import Empty from '../../components/empty/Empty'
 import { useSelector } from 'react-redux'
+
 function AllUsers() {
+    
   let users = useSelector(state => state.users.value)
   return (
     <div className='all__users'>
+      
       {
         users.length ? <Users data={users}/> : <Empty/>
       }
